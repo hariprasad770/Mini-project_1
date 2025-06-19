a0ef68a5ac3e6e3b3eff170785a05b0f147ab0da
hariprasad770 <haripra0305@gmail.com>
2025-06-19 15:44:59 +0530

    Update complexes requests.py

--- complexes requests.py
+++ complexes requests.py
@@ -6,17 +6,17 @@ import json
 url = "https://api.sportradar.com/tennis/trial/v3/en/complexes.json?api_key=TDUn19qxZted5umEoH8GFezjVMuczVGPS69CsWlw"
 headers = {"accept": "application/json"}
 
-# Make the API request
+# API request
 response = requests.get(url, headers=headers)
 data = response.json()  # Parse JSON response
 
 # Step 2: Connect to SQL Server using pyodbc
 connection_string = (
     'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=LAPTOP-1T835K9N\WINCC;'  # Change to your SQL Server's address or instance name
-    'DATABASE=tennisdb;'  # Change to your target database
-    'UID=sa;'  # SQL login username (or remove for Windows Authentication)
-    'PWD=Harish@123;'  # SQL login password (or remove for Windows Authentication)
+    'SERVER=LAPTOP-1T835K9N\WINCC;'  # SQL Server's address 
+    'DATABASE=tennisdb;'  # Target database
+    'UID=sa;'  # SQL login username 
+    'PWD=Harish@123;'  # SQL login password
 )
 conn = pyodbc.connect(connection_string)
 cursor = conn.cursor()
